4bfd9868904b5a8b1f5a1d0ea08b8350ee5f916a
Teddy1404 <aryansinghchandel1404@gmail.com>
2023-03-31 13:25:08 +0530

    navbar content removed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,14 @@
 import './App.css';
 import Home from './components/Home/Home';
 import Navbar from './components/Nabvbar/Navbar';
-
+import About from './components/Aboutus/About';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
       <Home/>
-
+      <About/>
     </div>
   );
 }
---
+++ frontend/src/components/Aboutus/About.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './About.css'
+const About = () => {
+  return (
+   <>
+   <div className="about">
+   <span>About Us and Our Theme</span>
+   </div>
+   <div className="heading">
+    <div className="left">left</div>
+    <div className="right">right</div>
+   </div>
+   
+   </>
+  )
+}
+
+export default About
