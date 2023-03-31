99d39c5acf50009581ccc083d807869309e54834
Hridyansh Verma <97448645+GodBrand69@users.noreply.github.com>
2023-03-31 16:16:37 +0530

    Add files via upload

--- frontend/src/components/GuestLectures.jsx
+++ frontend/src/components/GuestLectures.jsx
@@ -1,4 +1,5 @@
 import { TitleText } from "./CustomTexts";
+import { motion } from "framer-motion";
 
 const GuestLectures = () => {
     return (  
@@ -7,7 +8,11 @@ const GuestLectures = () => {
             <div className="flex gap-10 mx-auto justify-center">
                 <div className="my-20 flex justify-center">
                     <div className="bg-white w-[300px] h-[550px] rounded-xl">
-                        <img src="Dr_Satheesh_Reddy.jpg" className="w-[90%] my-4 rounded-xl mx-auto h-[200px]" />
+                        <motion.img whileHover={{
+                    scale: 1.1,
+                    transition: { duration: 0.7 },
+                        }}
+                    whileTap={{ scale: 0.9 }}  src="Dr_Satheesh_Reddy.jpg" className="w-[90%] my-4 rounded-xl mx-auto h-[200px]" />
                         <p className="w-[90%] text-md text-left ml-5 my-2">
                         Was the chief guest for the inaugural evening. With his vast expeyin navigation and avionics Design for missile systems he demonstrated exemplary courage and leadership during pandemic.
                         </p><br />
@@ -27,7 +32,11 @@ const GuestLectures = () => {
                 </div>
                 
                     <div className="bg-white my-20 w-[300px] h-[550px] rounded-xl">
-                        <img src="Dr A.S. Pillai.jpg" className="w-[90%] my-4 rounded-xl mx-auto h-[200px]" />
+                        <motion.img whileHover={{
+                    scale: 1.1,
+                    transition: { duration: 0.7 },
+                        }}
+                    whileTap={{ scale: 0.9 }}  src="Dr A.S. Pillai.jpg" className="w-[90%] my-4 rounded-xl mx-auto h-[200px]" />
                         <p className="w-[90%] text-md text-left ml-5 my-2">
                         Former CEO and MD of BrahMos Aerospace and recipient of The Padma Shri and Padma Bhushan awards imparted wisdom on the subject of Disruptive Innovations and the Future during the inaugural evening.
                         </p>
@@ -46,9 +55,13 @@ const GuestLectures = () => {
                     </div>
                 
                     <div className="bg-white my-20 w-[300px] h-[550px] rounded-xl">
-                        <img src="Pete-Draper.jpg" className="w-[90%] my-4 rounded-xl mx-auto h-[200px]" />
+                        <motion.img whileHover={{
+                    scale: 1.1,
+                    transition: { duration: 0.7 },
+                        }}
+                    whileTap={{ scale: 0.9 }}  src="Pete-Draper.jpg" className="w-[90%] my-4 rounded-xl mx-auto h-[200px]" />
                         <p className="w-[90%] text-md text-left ml-5 my-2">
-                        Co-founder of Makuta, a multi winning effects facility was our honored guest for the inaugural evening. His vast experience and his wisdom were a big treat for all film and special effects fans.
+                            Co-founder of Makuta, a multi winning effects facility was our honored guest for the inaugural evening. His vast experience and his wisdom were a big treat for all film and special effects fans.
                         </p><br />
                         <button className="ml-5 my-2 text-white font-bold hover:bg-sky-800 duration-200 transition ease-in bg-sky-500 px-4 py-2 rounded-lg">View More</button>
                         <div className="flex ml-5 my-2">
